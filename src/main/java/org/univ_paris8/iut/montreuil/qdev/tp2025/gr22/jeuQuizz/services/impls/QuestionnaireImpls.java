4ca220742c64a079a482396aa5ba57e6f61177ab
VincentM775 <vincent77.matias77@gmail.com>
2025-06-15 23:25:45 +0200

    push

---
+++ src/main/java/org/univ_paris8/iut/montreuil/qdev/tp2025/gr22/jeuQuizz/services/impls/QuestionnaireImpls.java
@@ -0,0 +1,5 @@
+package org.univ_paris8.iut.montreuil.qdev.tp2025.gr22.jeuQuizz.services.impls;
+
+public class QuestionnaireImpls {
+}
+//mettre la méthode de chargementfichier qui se trouve de dichierscvBO
